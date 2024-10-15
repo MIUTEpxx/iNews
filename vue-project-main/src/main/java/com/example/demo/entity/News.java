e59c1fa956430dc25b6b5e949b07972128f8a00f
MIUTEpxx <131579339+MIUTEpxx@users.noreply.github.com>
2024-10-15 22:26:47 +0800

    修复bug-新闻类数据类型与数据库中的数据类型不匹配

--- vue-project-main/src/main/java/com/example/demo/entity/News.java
+++ vue-project-main/src/main/java/com/example/demo/entity/News.java
@@ -7,19 +7,9 @@ public class News {
     private Integer id;
     private String title;
     private String picurl;
-    private Date posttime;
+    private String posttime;
     private Integer hits;
     private Integer classid;
     private String author;
-    // 空构造方法可以省略，因为@Data会生成一个无参构造方法
-    // 带参数的构造方法（如果需要）
-    public News(Integer id, String title, String picurl, Date posttime, Integer hits, Integer classid, String author) {
-        this.id = id;
-        this.title = title;
-        this.picurl = picurl;
-        this.posttime = posttime;
-        this.hits = hits;
-        this.classid = classid;
-        this.author = author;
-    }
+
 }
