313a70d8bbcb843b5277d60d3b8ed2d2ccaa4c7b
MIUTEpxx <131579339+MIUTEpxx@users.noreply.github.com>
2024-10-15 17:46:44 +0800

    为后端已有代码添加注释

--- vue-project-main/src/main/java/com/example/demo/config/CorsConfig.java
+++ vue-project-main/src/main/java/com/example/demo/config/CorsConfig.java
@@ -6,22 +6,25 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+//@Configuration注解表示这是一个配置类，Spring容器会加载这个类中的所有@Bean定义的Bean。
 @Configuration
-public class CorsConfig {
+public class CorsConfig {//跨域请求的配置
 
-    // 当前跨域请求最大有效时长。这里默认1天
+    // 当前跨域请求的结果可以被缓存的时间。这里默认1天
     private static final long MAX_AGE = 24 * 60 * 60;
 
-    @Bean
+    @Bean//@Bean注解表示该方法返回的Bean将被Spring容器管理
     public CorsFilter corsFilter() {
+        //创建了UrlBasedCorsConfigurationSource和CorsConfiguration的实例，用于定义跨域规则。
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.addAllowedOrigin("http://localhost:8080"); // 1 设置访问源地址
-        corsConfiguration.addAllowedHeader("*"); // 2 设置访问源请求头
-        corsConfiguration.addAllowedMethod("*"); // 3 设置访问源请求方法
-        corsConfiguration.setMaxAge(MAX_AGE);
-        source.registerCorsConfiguration("/**", corsConfiguration); // 4 对接口配置跨域设置
-        return new CorsFilter(source);
+
+        corsConfiguration.addAllowedOrigin("http://localhost:8081"); // 1 设置允许访问源地址
+        corsConfiguration.addAllowedHeader("*"); // 2 设置访问源请求头:允许所有的请求头
+        corsConfiguration.addAllowedMethod("*"); // 3 设置访问源请求方法:允许所有的HTTP方法（GET, POST, PUT, DELETE等）。
+        corsConfiguration.setMaxAge(MAX_AGE);//设置预检请求的结果可以缓存的最大时间为MAX_AGE。
+        source.registerCorsConfiguration("/**", corsConfiguration); // 4 对接口配置跨域设置:将跨域配置应用到所有路径（/**表示项目中的所有路径）。
+        return new CorsFilter(source);//返回一个实例 该实例使用前面设置的跨域配置
     }
 }
 
--- vue-project-main/src/main/java/com/example/demo/controller/UserController.java
+++ vue-project-main/src/main/java/com/example/demo/controller/UserController.java
@@ -10,37 +10,38 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@RestController
-@RequestMapping("/user")
+@RestController//标记这个类为Spring MVC的控制器，并且其中的每个方法返回的对象都会直接序列化为HTTP响应体
+@RequestMapping("/user")//表示这个控制器处理的所有请求都会映射到以/user为前缀的路径
 public class UserController {
-    @Autowired
+    @Autowired//注解用于自动装配UserMapper的实例，以便在控制器中使用它们的方法
     private UserMapper userMapper;
 
-    @Autowired
+    @Autowired//注解用于自动装配UserService的实例，以便在控制器中使用它们的方法
     private UserService userService;
 
     //新增和修改
-    @PostMapping
-    public Integer save(@RequestBody User user) {
+    @PostMapping//注解标记这是一个处理HTTP POST请求的方法
+    public Integer save(@RequestBody User user) {//@RequestBod注解表示参数user应该从HTTP请求体中绑定
         //新增和更新
-        return userService.save(user);
+        return userService.save(user);//这个方法用于新增或更新用户信息，并调用UserService的save方法
     }
 
     //查询所有数据
-    @GetMapping
+    @GetMapping//注解标记这是一个处理HTTP GET请求的方法
     public List<User> findAll() {
         return userMapper.findAll();
+    }//返回所有用户的列表，通过调用UserMapper的findAll方法实现
+
+    @DeleteMapping("/{id}")//注解标记这是一个处理HTTP DELETE请求的方法，其中{id}是一个路径变量
+    public Integer delete(@PathVariable Integer id) {//@PathVariable注解表示参数id应该从URI路径中绑定
+        return userMapper.deleteById(id);//根据提供的用户ID删除用户，通过调用UserMapper的deleteById方法实现
     }
 
-    @DeleteMapping("/{id}")
-    public Integer delete(@PathVariable Integer id) {
-        return userMapper.deleteById(id);
-    }
-
-    @GetMapping("/page")
+    @GetMapping("/page")//注解标记这是一个处理HTTP GET请求的方法，用于分页查询 @RequestParam注解表示参数应该从请求的查询参数中绑定
     public Map<String, Object> findPage(@RequestParam Integer pageNum, @RequestParam Integer pageSize, @RequestParam String username,
                                         @RequestParam String email, @RequestParam String address) {
-        pageNum = (pageNum - 1) * pageSize;
+        pageNum = (pageNum - 1) * pageSize;//计算分页的起始位置
+        //获取分页数据和总记录数
         List<User> data = userMapper.selectPage(pageNum, pageSize, username,email,address);
         Integer total = userMapper.selectTotal(username,email,address);
         Map<String, Object> res = new HashMap<>();
